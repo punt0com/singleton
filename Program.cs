7995ad099e26f3b0b20348e2eedcdb6eb01c2230
Andre Gomez <andremgm1@hotmail.com>
2020-02-11 16:54:12 -0600

    the first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    class Program
+    {
+
+        public class Life
+        {
+
+          private static Life existance = new Life();
+            private Life() { }
+
+            public static Life begin
+            {
+
+                get { return existance; }
+            }
+
+
+            public void proofOfLife()
+            {
+
+                Console.WriteLine("It has begun");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            Life being = Life.begin;
+            being.proofOfLife();
+
+
+            Life anotherBeing = Life.begin;
+            being.proofOfLife();
+
+            Life.begin.proofOfLife();
+
+            Console.ReadLine();
+            //las clases singleton no se pueden volver a inicializar
+        }
+    }
+}
